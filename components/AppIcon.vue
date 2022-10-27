e508187a88e16b1d139bf7f9013db6ab20444549
Roman Pashnev <crsprt@gmail.com>
2022-10-27 20:28:52 +0800

    Добавить иконки стрелок

--- components/AppIcon.vue
+++ components/AppIcon.vue
@@ -7,25 +7,15 @@
     fill="none"
     xml:space="preserve"
     xmlns="http://www.w3.org/2000/svg"
-    v-html="iconPack[name]"
+    v-html="IconPath[name]"
   >
   </svg>
 </template>
 
 <script lang="ts" setup>
-import IconName from '@/constants/enum/IconName';
 import IconPath from '@/constants/enum/IconPath';
 
-const iconPack = {
-  [IconName.ADD]: IconPath.ADD,
-  [IconName.CLOSE_CIRCLE]: IconPath.CLOSE_CIRCLE,
-  [IconName.CLOSE_SQUARE]: IconPath.CLOSE_SQUARE,
-  [IconName.INSTAGRAM]: IconPath.INSTAGRAM,
-  [IconName.MENU]: IconPath.MENU,
-  [IconName.SEND_2]: IconPath.SEND_2,
-};
-
-defineProps<{ name: keyof typeof iconPack; }>();
+defineProps<{ name: keyof typeof IconPath; }>();
 </script>
 
 <style lang="scss" scoped>
