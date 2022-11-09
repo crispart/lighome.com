893076dcd496182e2b0d9a252bc75236a2f3cf4f
Roman Pashnev <crsprt@gmail.com>
2022-11-09 22:25:10 +0800

    Заменить страницы ошибок

---
+++ error.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="error">
+    <div class="error__not-found">
+      <h1 class="error__not-found__message">Page Not Found</h1>
+      <NuxtLink to="/">Come back home</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useHead } from '@vueuse/head';
+
+useHead({ title: 'Not Found | Anastasia Tyuleneva' });
+</script>
+
+<style lang="scss" scoped>
+@use "@/assets/sass/fragments/palette";
+
+.error {
+  display: flex;
+  height: 100%;
+
+  &__not-found {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding: 0 40px;
+
+    &__message {
+      font-size: 48px;
+      color: palette.$text-primary;
+    }
+  }
+}
+</style>
