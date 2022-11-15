f83be0c1636e3a822ae764f9faca3fe9b1c5f54d
Roman Pashnev <crsprt@gmail.com>
2022-11-15 20:33:06 +0800

    Скрыть проекты, находящиеся в разработке

--- constants/DesignProject.ts
+++ constants/DesignProject.ts
@@ -1,12 +1,6 @@
 import ProjectInterface from '~/models/interfaces/ProjectInterface';
 
 const DesignProject: Array<ProjectInterface> = [
-  // {
-  //   path: 'urban_trend',
-  //   name: 'Urban Trend',
-  //   sphere: 'Identity',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  // },
   {
     path: 'coat-of-arms',
     name: 'Coat of arms',
@@ -35,193 +29,199 @@ const DesignProject: Array<ProjectInterface> = [
       },
     ],
   },
-  {
-    path: 'merkaba',
-    name: 'Merkaba',
-    sphere: 'Identity',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    images: [
-      {
-        name: 'image-00',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-01',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-02',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-03',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-04',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-05',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-06',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-07',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-08',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-09',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-10',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-11',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-12',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-13',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-14',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-15',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-16',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-17',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-18',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-19',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-20',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-21',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-22',
-        description: 'Merkaba',
-        size: 6,
-      },
-      {
-        name: 'image-23',
-        description: 'Merkaba',
-        size: 6,
-      },
-    ],
-  },
-  {
-    path: 'hipo',
-    name: 'Hipo',
-    sphere: 'Print',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    images: [
-      {
-        name: 'image-01',
-        description: 'Postcard',
-        size: 12,
-      },
-      {
-        name: 'image-02',
-        description: 'Postcard',
-        size: 6,
-      },
-      {
-        name: 'image-03',
-        description: 'Postcard',
-        size: 6,
-      },
-    ],
-  },
-  {
-    path: 'postcard',
-    name: 'Postcard',
-    sphere: 'Print',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    images: [
-      {
-        name: 'image-01',
-        description: 'Postcard',
-        size: 12,
-      },
-    ],
-  },
-  {
-    path: 'composition',
-    name: 'Composition',
-    sphere: 'Collage',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    images: [
-      {
-        name: 'image-01',
-        description: 'Composition',
-        size: 4,
-      },
-      {
-        name: 'image-02',
-        description: 'Composition',
-        size: 4,
-      },
-      {
-        name: 'image-03',
-        description: 'Composition',
-        size: 4,
-      },
-    ],
-  },
+  // {
+  //   path: 'urban_trend',
+  //   name: 'Urban Trend',
+  //   sphere: 'Identity',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  // },
+  // {
+  //   path: 'merkaba',
+  //   name: 'Merkaba',
+  //   sphere: 'Identity',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  //   images: [
+  //     {
+  //       name: 'image-00',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-01',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-02',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-03',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-04',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-05',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-06',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-07',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-08',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-09',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-10',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-11',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-12',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-13',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-14',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-15',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-16',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-17',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-18',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-19',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-20',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-21',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-22',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-23',
+  //       description: 'Merkaba',
+  //       size: 6,
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: 'hipo',
+  //   name: 'Hipo',
+  //   sphere: 'Print',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  //   images: [
+  //     {
+  //       name: 'image-01',
+  //       description: 'Postcard',
+  //       size: 12,
+  //     },
+  //     {
+  //       name: 'image-02',
+  //       description: 'Postcard',
+  //       size: 6,
+  //     },
+  //     {
+  //       name: 'image-03',
+  //       description: 'Postcard',
+  //       size: 6,
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: 'postcard',
+  //   name: 'Postcard',
+  //   sphere: 'Print',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  //   images: [
+  //     {
+  //       name: 'image-01',
+  //       description: 'Postcard',
+  //       size: 12,
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: 'composition',
+  //   name: 'Composition',
+  //   sphere: 'Collage',
+  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  //   images: [
+  //     {
+  //       name: 'image-01',
+  //       description: 'Composition',
+  //       size: 4,
+  //     },
+  //     {
+  //       name: 'image-02',
+  //       description: 'Composition',
+  //       size: 4,
+  //     },
+  //     {
+  //       name: 'image-03',
+  //       description: 'Composition',
+  //       size: 4,
+  //     },
+  //   ],
+  // },
   // {
   //   path: 'whiskey',
   //   name: 'Whiskey',
