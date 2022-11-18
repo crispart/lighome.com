db27607a0b6b4348deb5de9a8e8c17680668afdc
Roman Pashnev <crsprt@gmail.com>
2022-11-18 23:04:08 +0800

    Добавить проекты "Merkaba" и "Wine"

--- constants/DesignProject.ts
+++ constants/DesignProject.ts
@@ -1,10 +1,12 @@
 import ProjectInterface from '~/models/interfaces/ProjectInterface';
+import ProjectSphere from '~/constants/enum/ProjectSphere';
 
 const DesignProject: Array<ProjectInterface> = [
   {
     path: 'coat-of-arms',
     name: 'Coat of arms',
-    sphere: 'Logotype · 2022',
+    sphere: ProjectSphere.LOGOTYPE,
+    year: 2022,
     description: 'Behind a person is history. The coat of arms may contain visual information about what is behind this story. I have created a coat of arms to decorate things that are dear to my heart. You can order to design your personal or family’s coat of arms.',
     images: [
       {
@@ -32,7 +34,8 @@ const DesignProject: Array<ProjectInterface> = [
   {
     path: 'she',
     name: 'She Underwear',
-    sphere: 'Identity · 2022',
+    sphere: ProjectSphere.IDENTITY,
+    year: 2022,
     description: 'Identity for a young bright underwear brand that began its history under the auspices of a bright lifestyle, courage, openness in style. At the same time, the brand is sensual and gentle. I tried to keep this contrast in identity. Enjoy your viewing!',
     images: [
       {
@@ -162,208 +165,415 @@ const DesignProject: Array<ProjectInterface> = [
       },
     ],
   },
-  // {
-  //   path: 'urban_trend',
-  //   name: 'Urban Trend',
-  //   sphere: 'Identity',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  // },
-  // {
-  //   path: 'merkaba',
-  //   name: 'Merkaba',
-  //   sphere: 'Identity',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  //   images: [
-  //     {
-  //       name: 'image-00',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-01',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-02',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-03',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-04',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-05',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-06',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-07',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-08',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-09',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-10',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-11',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-12',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-13',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-14',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-15',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-16',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-17',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-18',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-19',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-20',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-21',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-22',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-23',
-  //       description: 'Merkaba',
-  //       size: 6,
-  //     },
-  //   ],
-  // },
+  {
+    path: 'urban_trend',
+    name: 'Urban Trend',
+    sphere: ProjectSphere.IDENTITY,
+    year: 2022,
+    description: 'Identity and branding for a brand that creates high-quality hair products.',
+    images: [
+      {
+        name: 'image-01',
+        description: 'Urban Trend',
+        size: 6,
+      },
+      {
+        name: 'image-02',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-03',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-04',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-05',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-06',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-07',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-08',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-09',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-10',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-11',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-12',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-13',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-14',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-15',
+        description: 'Urban Trend',
+        size: 6,
+      },
+      {
+        name: 'image-16',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-17',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-18',
+        description: 'Urban Trend',
+        size: 6,
+      },
+      {
+        name: 'image-19',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-20',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-21',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-22',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-23',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-24',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-25',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-26',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-27',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-28',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-29',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-30',
+        description: 'Urban Trend',
+        size: 6,
+      },
+      {
+        name: 'image-31',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-32',
+        description: 'Urban Trend',
+        size: 6,
+      },
+      {
+        name: 'image-33',
+        description: 'Urban Trend',
+        size: 6,
+        hasMask: true,
+      },
+      {
+        name: 'image-34',
+        description: 'Urban Trend',
+        size: 6,
+      },
+      {
+        name: 'image-35',
+        description: 'Urban Trend',
+        size: 6,
+      },
+      {
+        name: 'image-36',
+        description: 'Urban Trend',
+        size: 6,
+      },
+    ],
+  },
+  {
+    path: 'fragile-dreams',
+    name: 'Fragile dreams',
+    sphere: ProjectSphere.COLLAGE,
+    year: 2021,
+    description: 'Series of collages on the theme of dreams.',
+    images: [
+      {
+        name: 'image-01',
+        description: 'Fragile dreams',
+        size: 4,
+      },
+      {
+        name: 'image-02',
+        description: 'Fragile dreams',
+        size: 4,
+      },
+      {
+        name: 'image-03',
+        description: 'Fragile dreams',
+        size: 4,
+      },
+    ],
+  },
+  {
+    path: 'merkaba',
+    name: 'Merkaba',
+    sphere: ProjectSphere.IDENTITY,
+    year: 2020,
+    description: 'Merkaba is an electronic music festival for which I created a identity. I tried to convey the mood of the festival in the identity as much as possible and solve the tasks that were set by the organizers.',
+    images: [
+      {
+        name: 'image-00',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-01',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-02',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-03',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-04',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-05',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-06',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-07',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-08',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-09',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-10',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-11',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-12',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-13',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-14',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-15',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-16',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-17',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-18',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-19',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-20',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-21',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-22',
+        description: 'Merkaba',
+        size: 6,
+      },
+      {
+        name: 'image-23',
+        description: 'Merkaba',
+        size: 6,
+      },
+    ],
+  },
+  {
+    path: 'hermitage-wine',
+    name: 'Hermitage wine',
+    sphere: ProjectSphere.PACKAGING,
+    year: 2020,
+    description: 'Wine label design.',
+    images: [
+      {
+        name: 'image-01',
+        description: 'Hermitage wine',
+        size: 12,
+      },
+      {
+        name: 'image-02',
+        description: 'Hermitage wine',
+        size: 6,
+      },
+      {
+        name: 'image-03',
+        description: 'Hermitage wine',
+        size: 6,
+      },
+    ],
+  },
   // {
   //   path: 'hipo',
   //   name: 'Hipo',
   //   sphere: 'Print',
   //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  //   images: [
-  //     {
-  //       name: 'image-01',
-  //       description: 'Postcard',
-  //       size: 12,
-  //     },
-  //     {
-  //       name: 'image-02',
-  //       description: 'Postcard',
-  //       size: 6,
-  //     },
-  //     {
-  //       name: 'image-03',
-  //       description: 'Postcard',
-  //       size: 6,
-  //     },
-  //   ],
   // },
   // {
   //   path: 'postcard',
   //   name: 'Postcard',
   //   sphere: 'Print',
   //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  //   images: [
-  //     {
-  //       name: 'image-01',
-  //       description: 'Postcard',
-  //       size: 12,
-  //     },
-  //   ],
-  // },
-  // {
-  //   path: 'composition',
-  //   name: 'Composition',
-  //   sphere: 'Collage',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  //   images: [
-  //     {
-  //       name: 'image-01',
-  //       description: 'Composition',
-  //       size: 4,
-  //     },
-  //     {
-  //       name: 'image-02',
-  //       description: 'Composition',
-  //       size: 4,
-  //     },
-  //     {
-  //       name: 'image-03',
-  //       description: 'Composition',
-  //       size: 4,
-  //     },
-  //   ],
   // },
   // {
   //   path: 'whiskey',
   //   name: 'Whiskey',
-  //   sphere: 'Packaging',
-  //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-  // },
-  // {
-  //   path: 'wine',
-  //   name: 'Hermitage',
   //   sphere: 'Packaging',
   //   description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
   // },
